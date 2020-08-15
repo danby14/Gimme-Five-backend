f9dbac8061f8aee1e7edfefb6473549b31a5599b
danby14 <51226470+danby14@users.noreply.github.com>
2020-08-15 09:58:53 -0500

    removed unnecessary parse from production knex connection

--- knexfile.js
+++ knexfile.js
@@ -21,7 +21,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: JSON.parse(process.env.DATABASE_URL),
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: './data/migrations',
     },
